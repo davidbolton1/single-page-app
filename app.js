5c36120029f3876c70a5c05ed12fd7aeefadf188
David Bolton <39308057+davidbolton1@users.noreply.github.com>
2021-05-12 11:51:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const domain = window.env.AUTH0_DOMAIN;
 const client_id = window.env.CLIENT_ID;
 const redirect_uri = window.env.APP_URL;
  
-window.auth0Client = await createAuth0Client({
- domain,
- client_id,
- redirect_uri,
-});
+    window.auth0Client = await createAuth0Client({
+        domain,
+        client_id,
+        redirect_uri,
+        cacheLocation: "localstorage",
+        audience: "https://expenses-api", // 👈 Added
+        scope: "read:reports", // 👈 Added
+    });
+ 
   // handle user navigation
   window.addEventListener("hashchange", router);
   window.addEventListener("load", router);
